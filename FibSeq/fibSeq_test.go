a29ca21c0b7edfa458855b97b6dbca4bc1134dee
MikeMDV <mmarven09@gmail.com>
2020-09-29 14:38:36 -0400

    Updated unit tests for Fib()

--- FibSeq/fibSeq_test.go
+++ FibSeq/fibSeq_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"testing"
 )
 
@@ -10,7 +9,6 @@ func TestFib(t *testing.T) {
 		name        string
 		n           int64
 		expectedFib int64
-		expectedErr error
 	}
 
 	var testCases = []testCase{
@@ -18,47 +16,40 @@ func TestFib(t *testing.T) {
 			name:        "Test 0th fib number",
 			n:           0,
 			expectedFib: 0,
-			expectedErr: nil,
 		},
 		testCase{
 			name:        "Test 1st fib number",
 			n:           1,
 			expectedFib: 1,
-			expectedErr: nil,
 		},
 		testCase{
 			name:        "Test 2nd fib number",
 			n:           2,
 			expectedFib: 1,
-			expectedErr: nil,
 		},
 		testCase{
 			name:        "Test 3rd fib number",
 			n:           3,
 			expectedFib: 2,
-			expectedErr: nil,
 		},
 		testCase{
 			name:        "Test 4th fib number",
 			n:           4,
 			expectedFib: 3,
-			expectedErr: nil,
 		},
 		testCase{
 			name:        "Test 75th fib number",
-			n:           4,
+			n:           75,
 			expectedFib: 2111485077978050,
-			expectedErr: nil,
 		},
 		testCase{
 			name:        "Test negative number for n",
 			n:           -1,
 			expectedFib: 0,
-			expectedErr: errors.New("Error: n is less than 0"),
 		},
 	}
 
-	for _, testCase := range testCases {
+	for i, testCase := range testCases {
 		fib, err := Fib(testCase.n)
 
 		// Uncomment the lines below for debug purposes
@@ -66,9 +57,14 @@ func TestFib(t *testing.T) {
 		fmt.Println("fib received:", fib)
 		fmt.Println("err received:", err)*/
 
-		if fib != testCase.expectedFib && err != testCase.expectedErr {
-			t.Fatalf("Test, '%v', failed. Expected %d for fib number and received %d. Expected %v for err and received %v.",
-				testCase.name, testCase.expectedFib, fib, testCase.expectedErr, err)
+		if i < len(testCases)-1 {
+			if fib != testCase.expectedFib || err != nil {
+				t.Fatalf("Test, '%v', failed. Expected %d for fib number and received %d. Received err, %v.",
+					testCase.name, testCase.expectedFib, fib, err)
+			}
+		} else if fib != testCase.expectedFib || err == nil {
+			t.Fatalf("Test, '%v', failed. Expected %d for fib number and received %d. Received err, %v.",
+				testCase.name, testCase.expectedFib, fib, err)
 		}
 	}
 }
